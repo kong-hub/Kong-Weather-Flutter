338b429a7feddc55029ec330d3faca756c27f494
Sorawit Trutsat <sorawit@central.tech>
2021-04-05 20:26:06 +0700

    Added weather state failure

---
+++ lib/states/weather_state.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:weather_repo_flutter/models/weather.dart';
+
+abstract class WeatherState extends Equatable {
+  const WeatherState();
+  @override
+  List<Object> get props => [];
+}
+class WeatherStateInitial extends WeatherState { }
+
+class WeatherStateLoading extends WeatherState { }
+
+class WeatherStateSuccess extends WeatherState {
+  final Weather weather;
+  const WeatherStateSuccess({@required this.weather}): assert(weather != null);
+  @override
+  List<Object> get props => [weather];
+}
+
+class WeatherStateFailure extends WeatherState { }
