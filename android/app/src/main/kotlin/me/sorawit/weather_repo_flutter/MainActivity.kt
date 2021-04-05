343b983709b576026a884de59b92e65fe47ac373
Sorawit Trutsat <sorawit@central.tech>
2021-04-05 16:11:24 +0700

    first commit

---
+++ android/app/src/main/kotlin/me/sorawit/weather_repo_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package me.sorawit.weather_repo_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/weather.dart
@@ -0,0 +1,1 @@
+class Weather extends Equal
